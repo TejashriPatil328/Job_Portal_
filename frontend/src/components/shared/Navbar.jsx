d51306fe8ecdaff268c0d42127270f10cec97c0c
Tejashri Patil <your-github-tejashrip325@gmail.com>
2026-01-07 02:06:02 +0530

    Frontend setup and added Navbar component

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,71 @@
+import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover'
+import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
+
+import React from 'react'
+import { Button } from '../ui/button'
+import { LogOut, User2 } from 'lucide-react';
+
+export default function Navbar() {
+    const user = false;
+    return (
+
+        <div className='bg-white'>
+            <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
+                <div>
+                    <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Portal</span></h1>
+                </div>
+                <div className='flex items-center gap-12'>
+                    <ul className='flex font-medium items-center gap-5'>
+                        <li>Home</li>
+                        <li>Jobs</li>
+                        <li>Browse</li>
+                    </ul>
+                    {
+                        !user ? (
+                            <div className='flex items-center gap-2'>
+                                <Button variant="outline">Login</Button>
+                                <Button className='bg-[#6A38C2] hover:[#5b30a6'>Sign Up</Button>
+                            </div>
+                        )
+                            :
+                            (
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Avatar className='cursor-pointer'>
+                                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+
+                                        </Avatar>
+                                    </PopoverTrigger>
+                                    <PopoverContent variant="outline" className='w-80 m-2 '>
+                                        <div className='flex gap-4 space-y-2 p-2'>
+                                            <Avatar className='cursor-pointer'>
+                                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                            </Avatar>
+                                            <div>
+                                                <h4 className='font-medium'>Patil Kalpesh</h4>
+                                                <p className='text-sm text-muted-foreground'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error earum qui, </p>
+                                            </div>
+                                        </div>
+                                        <div className='flex flex-col my-2 text-gray-600'>
+                                            <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                                                <User2 />
+                                                <Button variant='link' className='border-0'>View Profile</Button>
+                                            </div>
+
+                                            <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                                                <LogOut />
+                                                <Button variant='link' className='border-0'>Logout</Button>
+                                            </div>
+                                        </div>
+                                    </PopoverContent>
+                                </Popover>
+                            )
+                    }
+
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
