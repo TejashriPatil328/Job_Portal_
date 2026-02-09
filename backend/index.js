a874a6e711598e44cfcf1b1c6ba6ebc0f0a573b8
Tejashri Patil <your-github-tejashrip325@gmail.com>
2026-02-10 00:38:52 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://job-hunt-lk14.onrender.com",
   credentials: true
 };
 
@@ -45,7 +45,6 @@ app.use((req, res) => {
 });
 
 
-//"http://localhost:8000/api/v1/user"
 app.listen(PORT, () => {
     connectDB();
   console.log(`Server is running on port ${PORT}`);
